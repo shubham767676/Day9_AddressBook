6dabb510299c01ece53b3e3782e96c636760e07c
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-21 21:59:16 +0530

    Added: Welcome Message

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,6 +4,7 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
+		// ArrayList<Contacts> person=new ArrayList<>();
 	}
 
 }
